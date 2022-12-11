90b2433a1afa89877f3ea75a489ddcb7a3d349d2
DEFFO DENIS <yvesdeffo@gmail.com>
2022-12-11 18:53:48 -0500

    Complete ecs-ecr-codebuild article part 1

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ module.exports = {
   projectId: "jtq8df",
 
   e2e: {
-    baseUrl: 'https://yvesdenis.github.io/',
     video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,41 @@
 describe('Test if the website behaves as expected', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit(Cypress.env('URL'))
   })
 
   it('Check if it is running in production mode', () => {
-    cy.get('[name="robots"]')
-      .should('have.attr','content','index, follow')
+    if(Cypress.env('URL') == "http://localhost:1313/") {
+      cy.get('[name="robots"]')
+        .should('have.attr','content','noindex, nofollow')
+    } else {
+      cy.get('[name="robots"]')
+        .should('have.attr','content','index, follow')
+    }
+  })
+
+  it('Check if main page title is rendered', () => {
+    cy.get('a').contains('Time-to-Geek') 
+  })
+
+  it('Check if About,articles,Contact sections are visible', () => {
+    cy.get('ul.pl0.mr3').children().should('have.length', 3) 
+  })
+
+  it('Check if the title when the website starts is Time to Geek | Time-to-Geek', () => {
+    cy.title().should('eq', 'Time to Geek | Time-to-Geek')
+  })
+
+  it('Check if Github link is rendered', () => {
+    console.log(cy.get('div.ananke-socials').first())
+    cy.get('div.ananke-socials').find('a').should('have.attr','href','https://github.com/yvesDenis')
+  })
+
+  it('Check if The number of artcles is correct', () => {
+    cy.get('[title="Articles page"]').click()
+
+    cy.get('h1').contains('Articles')
+
+    cy.get('aside').children().should('have.length', 2) 
+
   })
 })
